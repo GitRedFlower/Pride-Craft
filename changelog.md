7bce771de5cdcc50c7395dd7c5b99260388ceefd
GitALCH <theredflower19999@gmail.com>
2025-11-03 21:50:53 -0500

    V2.2.2

--- changelog.md
+++ changelog.md
@@ -1,12 +1,6 @@
-Changes:
-
-    Updated NeoForge Version
-    Wall Flags Are now a 2x2 recipe instead of a 3x3 recipe
     
 Fixes:
 
-    Mini Flags Now have a better hitbox
-    Flag Carpets now render better overall
-    Mini Flags now render better overall
-    Wall Flags now render better overall
+    Fixed extra space in Block of Unknown Material name
+    Minor Code Changes
 
--- src/main/java/net/redflower/pridecraft/datagen/ModItemModelProvider.java
+++ src/main/java/net/redflower/pridecraft/datagen/ModItemModelProvider.java
@@ -1,11 +1,8 @@
 package net.redflower.pridecraft.datagen;
 
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
-import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-import net.neoforged.neoforge.registries.DeferredItem;
 import net.redflower.pridecraft.PrideCraft;
 import net.redflower.pridecraft.item.ModItems;
 
@@ -50,11 +47,5 @@ public class ModItemModelProvider extends ItemModelProvider {
         //Other Items
         basicItem(ModItems.UNKNOWN_MATERIAL.get());
         basicItem(ModItems.CORRUPTED_FLAG_MATERIAL.get());
-    }
-
-    private ItemModelBuilder handheldItem(DeferredItem<?> item) {
-        return withExistingParent(item.getId().getPath(),
-                ResourceLocation.parse("item/handheld")).texture("layer0",
-                ResourceLocation.fromNamespaceAndPath(PrideCraft.MODID, "item/" + item.getId().getPath()));
     }
 }
